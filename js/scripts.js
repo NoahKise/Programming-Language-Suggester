a38ee000ebafe97930751c7a7efd6155a57b4a1a
noahkise <noah@kisefamily.com>
2023-10-20 14:07:35 -0700

    fix variable collection bug

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,23 @@ window.onload = function() {
     event.preventDefault();
     hideResults();
 
-    const color = document.getElementById("alienTree").value;
+    const color = document.getElementById("inputAlienTree").value;
     const topping1 = document.querySelector("select#topping1").value;
     const topping2 = document.querySelector("select#topping2").value;
     const topping3 = document.querySelector("select#topping3").value;
     const ranch = document.querySelector("select#ranch").value;
-    const crisisAge = parseInt(document.getElementById("crisis").value)
+    const crisisAge = parseInt(document.getElementById("inputCrisis").value)
     const matthew = document.querySelector("input#matthew:checked").value;
-    const joke = document.getElementById("joke").value;
+    const joke = document.getElementById("inputJoke").value;
 
     if (topping1 === "char-and-cup-pepperoni" && topping2 === "hot-pickled-peppers" && topping3 === "green-olives") {
       friends.removeAttribute("class");
     } else if (topping1 === "bacon" && topping2 === "red-onions" && topping3 === "pineapple") {
       girlfriend.removeAttribute("class");
-    // } else if (ranch === "true") {
-    //   ruby.removeAttribute("class");
+    } else if (matthew === "manic") {
+      javaScript.removeAttribute("class");
+    } else if (topping1 === "pepperoni" || topping1 === "char-and-cup-pepperoni") {
+      ruby.removeAttribute("class");
     }
   });
 };
